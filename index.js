1947e05fe91547eaa1083c9246a55e72f62bff4d
Sam Ewaleifoh <sam2500@me.com>
2023-08-02 21:22:29 -0400

    Learning JS

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let firstName = "Sam";
+let age = 21;
+age = age + 1;
+
+// console.log("Hello " + firstName + " you are " + age);
+
+console.log ("Hello " + firstName);
+
+
+document.getElementById("");
