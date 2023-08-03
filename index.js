92c08cfa643ac2d915b37d415c66a0a86a3d407a
Sam Ewaleifoh <sam2500@me.com>
2023-08-02 21:52:49 -0400

    learn 1 line of code lol

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
-let firstName = "Sam";
-let age = 21;
-age = age + 1;
+// let firstName = "Sam";
+// let age = 21;
+// age = age + 10;
 
-// console.log("Hello " + firstName + " you are " + age);
+// // console.log("Hello " + firstName + " you are " + age);
 
-console.log ("Hello " + firstName);
+// console.log ("Hello " + firstName);
+
+// document.getElementById("p1").innerHTML = "Hello " + firstName;
+// document.getElementById("p2").innerHTML = "You are " + age + " years old.";
+
+let students = 20;
+students = students + 1;
 
 
-document.getElementById("");
+console.log(students);
